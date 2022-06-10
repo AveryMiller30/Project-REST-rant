e3d8d2a8a439b68a6a797aa6441a7a5ae4bad35d
AveryMiller30 <averymiller30@gmail.com>
2022-06-09 20:11:37 -0500

    images

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,7 +7,7 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <div>
-              <img src="/images/CORVETTE.jpg" alt="corvette"></img>
+              <img src="/images/cat.jpg" alt="cat"></img>
               </div>
               <p>Oops, sorry, we can't find this page!</p>
           </main>

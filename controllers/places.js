f6493e2a832341ac4b0939bc0aacc0953aa125c5
AveryMiller30 <averymiller30@gmail.com>
2022-06-26 10:11:13 -0500

    part10

--- controllers/places.js
+++ controllers/places.js
@@ -1,33 +1,40 @@
 const router = require('express').Router()
 const db = require('../models')
 
-router.get('/', (req, res) => {
-    db.Place.find()
-    .then((places) => {
-      res.render('places/index', { places })
-    })
-    .catch(err => {
-      console.log(err) 
-      res.render('error404')
-    })
-})
-
-router.post('/', (req, res) => {
-  db.Place.create(req.body)
-  .then(() => {
-      res.redirect('/places')
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+  .populate('comments')
+  .then(place => {
+      console.log(place.comments)
+      res.render('places/show', { place })
   })
   .catch(err => {
-      if (err && err.name == 'ValidationError') {
-          // TODO: Generate error message(s)
-      }
-      else {
-          res.render('error404')
-      }
+      console.log('err', err)
+      res.render('error404')
   })
 })
 
 
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
 
 
 router.get('/new', (req, res) => {
